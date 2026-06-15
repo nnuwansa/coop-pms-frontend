067dbbf2e26ce2d91fdc2af33c531439319c8c85
nnuwansa <nadininuwansa@gmail.com>
2026-06-15 17:29:15 +0530

    Updated

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -13,21 +13,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog"
 
-// function Command({
-//   className,
-//   ...props
-// }: React.ComponentProps<typeof CommandPrimitive>) {
-//   return (
-//     <CommandPrimitive
-//       data-slot="command"
-//       className={cn(
-//         "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
-//         className
-//       )}
-//       {...props}
-//     />
-//   )
-// }
+
 
 function Command({
   className,
